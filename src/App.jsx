ad309e275300e5f7d7aee425375c7b82d8d1920d
mjh-shikder <mjhsikder@gmail.com>
2025-10-08 11:05:01 +0600

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SimpleForm from './Components/SimpleForm'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>Explore React Form</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/SimpleForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SimpleForm = () => {
+    return (
+        <div>
+            <h2>Hola</h2>
+        </div>
+    );
+};
+
+export default SimpleForm;
