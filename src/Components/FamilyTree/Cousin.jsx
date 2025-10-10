4e9fb72d0494eca1abbb176195e7a194d0557d0f
mjh-shikder <mjhsikder@gmail.com>
2025-10-10 20:38:29 +0600

    showing money to friend page

--- src/Components/FamilyTree/Cousin.jsx
+++ src/Components/FamilyTree/Cousin.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Special from './Special';
+import Friend from './Friend';
+import { MoneyContext } from './FamilyTree';
 
-const Cousin = ({name, asset}) => {
+const Cousin = ({ name, asset }) => {
+    
+   
+
+
     return (
         <div>
             <h2>{name}</h2>
 
             {
                 name === 'tom tom' && <Special asset={asset}></Special>
+            }
+            {
+                name === 'joo joo' && <Friend></Friend>
             }
         </div>
     );
---
+++ src/Components/FamilyTree/Friend.jsx
@@ -0,0 +1,18 @@
+import React, { use } from 'react';
+import { MoneyContext } from './FamilyTree';
+
+const Friend = () => {
+
+
+    const [money, setMoney] = use(MoneyContext);
+
+
+    return (
+        <div>
+            <h2>Friend</h2>
+            <p>Family got: {money} </p>
+        </div>
+    );
+};
+
+export default Friend;
